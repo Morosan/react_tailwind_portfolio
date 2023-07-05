943235e1751f2bbeb4be7d954297f896f6cc667c
Andrei M <98398587+andrei-wm@users.noreply.github.com>
2023-07-05 15:31:11 +0300

    Hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
-            Andrei &nbsp;
-            <span className='sm:block hidden'>| UI Engineer</span>
+            Lorem &nbsp;
+            <span className='sm:block hidden'>| Ipsum</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
